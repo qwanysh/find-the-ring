232d2d3576c90b72e3074213923a6c27aafd1d55
qwanysh <kuanysh.beisembayev@gmail.com>
2020-09-18 16:27:41 +0600

    Add ChestContainer, ControlPanel to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {getChests, countOpenedChests} from '../../utils';
+import ChestContainer from '../ChestContainer/ChestContainer';
+import ControlPanel from '../ControlPanel/ControlPanel';
 
-function App() {
+const App = () => {
+  const AMOUNT = 36;
+  const [chests, setChests] = useState(getChests(AMOUNT));
+  const openedChests = countOpenedChests(chests);
+
+  const openChest = index => {
+    const chestsCopy = [...chests];
+    chestsCopy[index].isOpen = true;
+    setChests(chestsCopy);
+  };
+
   return (
-    <div className='App'/>
+      <div className='App'>
+        <div className='container'>
+          <ChestContainer chests={chests} openChestHandler={openChest}/>
+          <ControlPanel openedChests={openedChests} totalChests={AMOUNT}/>
+        </div>
+      </div>
   );
 }
 
