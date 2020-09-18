6cecab4d9151a2e0fcb641d1612f192e3b6772ce
qwanysh <kuanysh.beisembayev@gmail.com>
2020-09-18 17:49:43 +0600

    Updates

---
+++ precache-manifest.f5b644ce6a1c1a88ddee8c1dcf69e664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f6c009627a9a660cba23185a6f823f",
+    "url": "/find-the-ring/index.html"
+  },
+  {
+    "revision": "1ef963940507b8e13140",
+    "url": "/find-the-ring/static/css/main.7f364bd2.chunk.css"
+  },
+  {
+    "revision": "320ec6d22dbe9a2e7002",
+    "url": "/find-the-ring/static/js/2.52138beb.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/find-the-ring/static/js/2.52138beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef963940507b8e13140",
+    "url": "/find-the-ring/static/js/main.654ccbcb.chunk.js"
+  },
+  {
+    "revision": "eb9ac6236d4745db2e90",
+    "url": "/find-the-ring/static/js/runtime-main.e5885e0b.js"
+  }
+]);
