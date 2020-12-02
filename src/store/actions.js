6bfc4680c4511c33a90b9b61e88885e559fc917f
qwanysh <olimpsdu@gmail.com>
2020-12-02 10:17:38 +0600

    Remove unused actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,20 +1,8 @@
-import {
-  CHANGE_GAME_STATUS,
-  SET_CHESTS,
-  OPEN_CHEST,
-  RESTART_GAME,
-} from './actionTypes';
+import { OPEN_CHEST, RESTART_GAME } from './actionTypes';
 
-export const changeGameStatus = (gameStatus) => ({
-  type: CHANGE_GAME_STATUS,
-  payload: gameStatus,
+export const openChest = (chestIndex) => ({
+  type: OPEN_CHEST,
+  payload: chestIndex,
 });
-
-export const setChests = (chests) => ({
-  type: SET_CHESTS,
-  payload: chests,
-});
-
-export const openChest = (chestIndex) => ({ type: OPEN_CHEST, payload: chestIndex });
 
 export const restartGame = () => ({ type: RESTART_GAME });
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,20 +1,9 @@
-import {
-  CHANGE_GAME_STATUS,
-  SET_CHESTS,
-  OPEN_CHEST,
-  RESTART_GAME,
-} from './actionTypes';
+import { OPEN_CHEST, RESTART_GAME } from './actionTypes';
 import { CHEST_COUNT, MAX_ATTEMPTS, GameStatus } from '../consts';
 import { createChests, countOpenedChests, getChestWithRing } from '../utils';
 
 const reducer = (state, { type, payload }) => {
   switch (type) {
-    case CHANGE_GAME_STATUS:
-      return { ...state, gameStatus: payload };
-
-    case SET_CHESTS:
-      return { ...state, chests: payload };
-
     case OPEN_CHEST:
       if (state.gameStatus === GameStatus.IN_PROGRESS) {
         const newState = { ...state };
